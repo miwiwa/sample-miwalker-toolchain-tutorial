2fe9e007ff59d43bd1e619ba5dfa62e31c0d2089
Michael Walker <miwalker@us.ibm.com>
2018-10-19 22:44:55 +0000

    remove string cast

--- create_incident.py
+++ create_incident.py
@@ -36,8 +36,8 @@ parser.add_argument('-s','--SERVICE_ID', nargs='+', dest='SERVICE_ID', help="Pag
 #parser.add_argument('-f','--EMAIL_FROM', nargs='+', dest='EMAIL_FROM', help="Add valid PagerDuty email address", required=True)
 
 args = parser.parse_args()
-api_key = str(args.API_KEY)
-service_id=str(args.SERVICE_ID)
+api_key = args.API_KEY
+service_id = args.SERVICE_ID
 #email_from=str(args.EMAIL_FROM)
 
 print "Checking types....."
