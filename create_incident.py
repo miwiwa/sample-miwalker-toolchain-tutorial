6efc19f92a8cf51f7362bf96c82aa67c6149059f
Michael Walker <miwalker@us.ibm.com>
2018-10-24 02:00:58 +0000

    never forget =

--- create_incident.py
+++ create_incident.py
@@ -115,7 +115,7 @@ def trigger_incident():
 
 if __name__ == '__main__':
 	print("performing recursive lookup")
-	keys = [x for x in d if d[x] = 'acbc82b3-4053-4218-9f20-6d8a0c82e3dfpagerduty']
+	keys = [x for x in d if d[x] == 'acbc82b3-4053-4218-9f20-6d8a0c82e3dfpagerduty']
 	print(keys)
 	#print "Value : %s" %  data.get("services")
 	#print (deep_get(data, "u'created'"))
