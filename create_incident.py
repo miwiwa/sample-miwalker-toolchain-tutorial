3945a9b597941e3c53b279f8379019543c936672
Michael Walker <miwalker@us.ibm.com>
2018-10-29 06:28:30 +0000

    h

--- create_incident.py
+++ create_incident.py
@@ -32,7 +32,10 @@ from os import environ
 
 #import requests
 
+package = 'requests'
+
 import pip
+pip.main(['install', package])
 
 def import_or_install(package):
     try:
@@ -42,8 +45,6 @@ def import_or_install(package):
     	print("Attempting to install " + package)
         pip.main(['install', package])
 
-import_or_install('requests')
-       
 ids_job_name = environ.get('IDS_JOB_NAME')
 ids_job_id = environ.get('IDS_JOB_ID')
 ids_stage_name = environ.get('IDS_STAGE_NAME')
