8d7961261700cd117cbabcb2307731bc5a4ec8bd
Michael Walker <miwalker@us.ibm.com>
2018-10-31 00:32:38 +0000

    m

--- create_git_issue.py
+++ create_git_issue.py
@@ -24,13 +24,12 @@ repo_owner = ''.join(args.REPO_OWNER)
 repo_name = ''.join(args.REPO_NAME)
 
 
-result = subprocess.check_output(['git','config','--get','remote.orgin.url'])
 
 
 #GIT_REMOTE_URL=$( git config --get remote.origin.url )
 ##GIT_USER=$( echo ${GIT_REMOTE_URL} | cut -d/ -f3 | cut -d: -f1 )
 #GIT_PASSWORD=$( echo ${GIT_REMOTE_URL} | cut -d: -f3 | cut -d@ -f1 )
-print("Result:", result)
+
 #print("GIT_REMOTE_URL:",GIT_REMOTE_URL)
 #print("GIT_USER:",GIT_USER)
 #print("GIT_PASSWORD:",GIT_PASSWORD)
