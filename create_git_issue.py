060a367975a9264786cb01a63ae1cc6b67466f34
Michael Walker <miwalker@us.ibm.com>
2018-11-01 04:57:52 +0000

    j

--- create_git_issue.py
+++ create_git_issue.py
@@ -36,42 +36,49 @@ with open(toolchain_json) as f:
 # Requires user to pass in 4 parameters:  Their Git username and password which must have read/write access to repo. In addition, the script
 # requires the Git repo and owner names
 
-parser = argparse.ArgumentParser(     description=__doc__)
+#parser = argparse.ArgumentParser(     description=__doc__)
 
-parser.add_argument('-u','--GIT_USERNAME', nargs='+', type=str, dest='GIT_USERNAME', help="Git userid to repo that has read/write access", required=True)
-parser.add_argument('-p','--GIT_PASSWORD', nargs='+', type=str, dest='GIT_PASSWORD', help="Git password to repo that has read/write access", required=True)
-parser.add_argument('-o','--REPO_OWNER', nargs='+', type=str, dest='REPO_OWNER', help="Owner of repo to create issue in", required=True)
-parser.add_argument('-n','--REPO_NAME', nargs='+', type=str, dest='REPO_NAME', help="Name of repo to create issue in", required=True)
+#parser.add_argument('-u','--GIT_USERNAME', nargs='+', type=str, dest='GIT_USERNAME', help="Git userid to repo that has read/write access", required=True)
+#parser.add_argument('-p','--GIT_PASSWORD', nargs='+', type=str, dest='GIT_PASSWORD', help="Git password to repo that has read/write access", required=True)
+#parser.add_argument('-o','--REPO_OWNER', nargs='+', type=str, dest='REPO_OWNER', help="Owner of repo to create issue in", required=True)
+#parser.add_argument('-n','--REPO_NAME', nargs='+', type=str, dest='REPO_NAME', help="Name of repo to create issue in", required=True)
 
-args = parser.parse_args()
-git_username = ''.join(args.GIT_USERNAME)
-git_password = ''.join(args.GIT_PASSWORD)
-repo_owner = ''.join(args.REPO_OWNER)
-repo_name = ''.join(args.REPO_NAME)
-
-
-git_remote_url = subprocess.check_output(['git','config','--get','remote.origin.url'],stderr= subprocess.STDOUT)
-
-pattern = re.compile(r"//|:|@")
-git_parameters = pattern.split(git_remote_url)
-print(type(git_parameters))
-print(git_parameters)
-
-git_username = git_parameters[2]
-git_password = git_parameters[3]
-
-print("git_username: " + git_username)
-print("git_password: " + git_password)
+#args = parser.parse_args()
+#git_username = ''.join(args.GIT_USERNAME)
+#git_password = ''.join(args.GIT_PASSWORD)
+#repo_owner = ''.join(args.REPO_OWNER)
+#repo_name = ''.join(args.REPO_NAME)
 
 
-pd_github = [i['parameters'] for i in data["services"] if 'github' in i['broker_id']]
-print("pd_github: ", pd_github)
+
 
 def trigger_issue(title, body=None, labels=None):
-    """Triggers an incident via the V2 REST API using sample data."""
-    
+   
+    git_remote_url = subprocess.check_output(['git','config','--get','remote.origin.url'],stderr= subprocess.STDOUT)
+
+	pattern = re.compile(r"//|:|@")
+	git_parameters = pattern.split(git_remote_url)
+	print(type(git_parameters))
+	print(git_parameters)
+
+	git_username = git_parameters[2]
+	git_password = git_parameters[3]
+
+	print("git_username: " + git_username)
+	print("git_password: " + git_password)
+    repo_owner = [i['parameters']['owner_id'] for i in data["services"] if 'pagerduty' in i['broker_id']]
+	repo_name = [i['parameters']['repo_name'] for i in data["services"] if 'pagerduty' in i['broker_id']]
+	#pd_github = [i['parameters'] for i in data["services"] if 'github' in i['broker_id']]
+	#print("pd_github: ", pd_github)
+
+	try:
+      git_repo_owner = repo_owner[0]
+      git_repo_name = repo_name[0]
+    except IndexError:
+      print("ERROR: Pager Duty is not configured correctly with the toolchain")
+
     # Specifies URL for github api
-    url = 'https://api.github.com/repos/%s/%s/issues' % (repo_owner, repo_name)
+    url = 'https://api.github.com/repos/%s/%s/issues' % (git_repo_owner, git_repo_name)
   
   	# Create an authenticated session to create the issue
     session = requests.Session()
