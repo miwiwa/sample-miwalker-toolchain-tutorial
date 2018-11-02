537f9de09da2dee8497bfa613ff925a46933d66c
Michael Walker <miwalker@us.ibm.com>
2018-11-02 00:09:50 +0000

    h

---
+++ wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+executable_script=$1
+incident=$2
+issue=$3
+
+qa_test=$(node $executable_script lint) > output.txt
+
+echo $(cat output.txt)
+echo $qa_test
+
+if [[ qa_test != 0]]; then
+	$(python create_alert.py -a $incident $issue)
+else
+	echo "tests passed"
+fi
